b1dbb403b01a140b9b311576a6b85d351772609c
xinnnix <zxgilbert@gmail.com>
2021-12-24 14:39:30 +0800

    update

---
+++ relearnPython/ex15.py
@@ -0,0 +1,18 @@
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print(f"Here's your file {filename}:")
+print(txt.read())
+
+print("Type the filename again:")
+fileAgain = input(">")
+
+txtAgain = open(fileAgain)
+
+print(txtAgain.read())
+
+txt.close()
+txtAgain.close()
---
+++ relearnPython/ex19.py
@@ -0,0 +1,22 @@
+def cheeseAndCrackers(cheeseCount, boxesOfCrackers):
+    print(f'You have {cheeseCount} cheeses!')
+    print(f'You have {boxesOfCrackers} boxes of crackers')
+    print("Man that's enough for a party!")
+    print('Get a blanket.\n')
+
+
+print("We can just give the function numbers directly:")
+cheeseAndCrackers(20, 30)
+
+print("Or, we can use variables from our script:")
+
+amountOfCheese = 10
+amountOfCrackers = 50
+
+cheeseAndCrackers(amountOfCheese, amountOfCrackers)
+
+print("We can even do math inside too:")
+cheeseAndCrackers(10 + 20, 5 + 6)
+
+print('And we can combine the two, variables and math:')
+cheeseAndCrackers(amountOfCheese + 100, amountOfCrackers + 1000)
